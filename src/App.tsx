b9c4d8b1125d963ffe38c533ecd7f16217e57f87
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2024-08-24 13:03:29 +0300

    Try alert in chrome extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 
 function App() {
+   // @ts-ignore
+   const onClick = async () => {
+      const [tab] = await chrome.tabs.query({ active: true })
+
+      if (!tab.id) {
+         alert("No tab id available")
+         return;
+      }
+
+      await chrome.scripting.executeScript({
+         target: { tabId: tab.id },
+         func: () => {
+            alert("My Extension!")
+         },
+      });
+   }
+
   return (
      <>
         <h1 className="text-center text-3xl font-bold underline">
-           Hello world!
+           Hello world!!!
         </h1>
+        <button onClick={() => onClick()}>Click!</button>
      </>
   )
 }
