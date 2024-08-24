08d734502561c007d71374cc43d16ecd3c186e38
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2024-08-24 23:57:56 +0300

    feat: spy example

--- src/store/darkModeAtom.ts
+++ src/store/darkModeAtom.ts
@@ -3,3 +3,9 @@ import {withLocalStorage} from "@reatom/persist-web-storage"
 
 export const darkModeAtom = atom(false, 'darkModeAtom')
     .pipe(withLocalStorage("darkModeAtom"));
+
+//
+// export const isUserLoading = atom(
+//   (ctx) => ctx.spy(darkModeAtom.pendingAtom) > 0,
+//   "getMeLoading",
+// );
